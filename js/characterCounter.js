f72e793eddbb96e1b04d5ed8702713fb0560ecfd
Ian Robinson <ian@ianrobinson.net>
2021-11-21 10:00:49 -0600

    Add user card

---
+++ js/characterCounter.js
@@ -0,0 +1,9 @@
+let countThis = "This would be where you would add some truncated text to link to a blog. This blob is ~150 characters so you can use this as a way to understand the space for the copy.";
+
+let characterCount = 0;
+
+for (let i = 0; i < countThis.length; i += 1) {
+  characterCount += 1;
+}
+
+console.log(characterCount);
